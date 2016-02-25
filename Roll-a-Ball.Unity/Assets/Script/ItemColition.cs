e94e8fff6c8579f18ec8856b816f8beb1bfae0ea
guitarrapc <cherishacquire@outlook.com>
2016-02-26 00:23:41 +0900

    Add Prefab

---
+++ Roll-a-Ball.Unity/Assets/Script/ItemColition.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+namespace Assets.Script
+{
+    public class ItemColition : MonoBehaviour
+    {
+        void OnTriggerEnter(Collider hit)
+        {
+            if (hit.CompareTag("Player"))
+            {
+                Destroy(gameObject);
+            }
+        }
+    }
+}
