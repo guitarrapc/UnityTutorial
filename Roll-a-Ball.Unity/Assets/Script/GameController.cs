b515a1d2057f181a361ba082593eab6e7fc44857
guitarrapc <cherishacquire@outlook.com>
2016-02-26 19:58:22 +0900

    Score と経過時間表示

--- Roll-a-Ball.Unity/Assets/Script/GameController.cs
+++ Roll-a-Ball.Unity/Assets/Script/GameController.cs
@@ -1,4 +1,5 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
 using UnityEngine.UI;
 
 namespace Assets.Script
@@ -6,16 +7,28 @@ namespace Assets.Script
     public class GameController : MonoBehaviour
     {
         public Text ScoreLabel;
+        public Text ElapsedTimeLabel;
         public GameObject WinnerLabel;
+        private int _initialItemCount;
+
+        public void Start()
+        {
+            _initialItemCount = GameObject.FindGameObjectsWithTag("Item").Length;
+        }
 
         public void Update()
         {
+            // 経過時間表示
+            var elapsedTime = TimeSpan.FromSeconds(double.Parse(Time.time.ToString()));
+            ElapsedTimeLabel.text = string.Format("経過時間 : {0:00}:{1:00}:{2:00}", elapsedTime.Minutes, elapsedTime.Seconds, elapsedTime.TotalMilliseconds);
+
             // 残数表示
-            var count = GameObject.FindGameObjectsWithTag("Item").Length;
-            ScoreLabel.text = count.ToString();
+            var itemCount = GameObject.FindGameObjectsWithTag("Item").Length;
+            var score = _initialItemCount - itemCount;
+            ScoreLabel.text = string.Format("Score : " + score);
 
             // クリア時の処理
-            if (count == 0)
+            if (itemCount == 0)
             {
                 WinnerLabel.SetActive(true);
             }
