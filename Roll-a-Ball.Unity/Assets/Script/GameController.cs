94ec03f5ab1724b3ad556656519014daed1b7d20
guitarrapc <cherishacquire@outlook.com>
2016-02-27 02:49:37 +0900

    制限時間

--- Roll-a-Ball.Unity/Assets/Script/GameController.cs
+++ Roll-a-Ball.Unity/Assets/Script/GameController.cs
@@ -1,19 +1,30 @@
 ﻿using System;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
 namespace Assets.Script
 {
     public class GameController : MonoBehaviour
     {
+        public float GameOverLimit;
         public Text ScoreLabel;
         public Text ElapsedTimeLabel;
-        public GameObject WinnerLabel;
+        public Text TimeLimitLabel;
+        public GameObject FinishLabel;
+
         private int _initialItemCount;
+        private float timeLimit;
+        private float deltaTimer;
 
         public void Start()
         {
             _initialItemCount = GameObject.FindGameObjectsWithTag("Item").Length;
+            FinishLabel.SetActive(false);
+            ScoreLabel.text = "Score : 0";
+            TimeLimitLabel.text = "TimeLimit : " + GameOverLimit;
+            ElapsedTimeLabel.text = "Elapsed time : 00:00:00";
+            timeLimit = GameOverLimit;
         }
 
         public void Update()
@@ -21,6 +32,11 @@ namespace Assets.Script
             // 経過時間表示
             var elapsedTime = TimeSpan.FromSeconds(double.Parse(Time.time.ToString()));
             ElapsedTimeLabel.text = string.Format("Elapsed time : {0:00}:{1:00}:{2:00}", elapsedTime.Minutes, elapsedTime.Seconds, elapsedTime.TotalMilliseconds);
+
+            // 制限時間表示
+            if (Time.time > timeLimit) timeLimit = 0;
+            timeLimit -= Time.deltaTime;
+            TimeLimitLabel.text = "TimeLimit : " + (int)timeLimit;
 
             // 残数表示
             var itemCount = GameObject.FindGameObjectsWithTag("Item").Length;
@@ -30,7 +46,27 @@ namespace Assets.Script
             // クリア時の処理
             if (itemCount == 0)
             {
-                WinnerLabel.SetActive(true);
+                FinishLabel.GetComponent<Text>().text = "You Win!!";
+                FinishLabel.SetActive(true);
+            }
+
+            // GameOver 処理
+            if (timeLimit <= 0)
+            {
+                FinishLabel.GetComponent<Text>().text = "Game Over....";
+                FinishLabel.SetActive(true);
+            }
+        }
+
+        public void OnGUI()
+        {
+            if (Time.time >= GameOverLimit)
+            {
+                if (GUILayout.Button("Reload"))
+                {
+                    FinishLabel.SetActive(false);
+                    SceneManager.LoadSceneAsync(SceneManager.GetActiveScene().name);
+                }
             }
         }
     }
